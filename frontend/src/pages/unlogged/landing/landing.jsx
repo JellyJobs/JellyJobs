c7dddea6e18df2405b80b17bb14f62a85f73a320
Vgarzaniti <145395841+Vgarzaniti@users.noreply.github.com>
2024-10-03 11:59:48 -0300

    landing page
    links imagenes en archivo jsx, modificacion de los componentes de header y footer

--- frontend/src/pages/unlogged/landing/landing.jsx
+++ frontend/src/pages/unlogged/landing/landing.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import { Layout, Button } from 'antd';
 import { AntDesignOutlined } from '@ant-design/icons';
 import {ConfigProvider, Space } from 'antd';
+import gmail from '../../../assets/images/gmail.webp';
+import twitter from '../../../assets/images/tw.png';
+import instagram from '../../../assets/images/inta.jpeg';
+import logo from  '../../../assets/images/logo.png';
 
 const { Header, Footer } = Layout;
 
@@ -10,7 +14,7 @@ export default function Landing() {
   return (
     <div>
       <Header className='header'>
-        <div className='logo-JellyJobs'><a href="/" className='links'></a></div>
+        <a href="/" className='links,logo-JellyJobs'><img src={logo} alt="logo" /></a>
         <ConfigProvider>
           <Space className='buttons-log'>
             <Button
@@ -35,10 +39,15 @@ export default function Landing() {
       
       <Footer className='footer'>
         {/** logos con links de informacion */}
-        {/*<h2>informacion</h2>*/}
-        <div className='logo-insta'><a href='https://www.instagram.com' className='links'></a></div>
-        <div className='logo-gmail'><a href='https://x.com' className='links'></a></div>
-        <div className='logo-tw'><a href='https://mail.google.com' className='links'></a></div>
+        <h2>informacion</h2>
+        <nav>
+          <a href='https://www.instagram.com/'><img src={instagram} alt="enlace instagram"  className='logo'/></a>
+          <a href='https://www.google.com/intl/es-419/gmail/about/'><img src={gmail} alt="enlace gmail" className='logo'/></a>
+          <a href='https://x.com/?lang=es'><img src={twitter} alt="enlace twitter" className='logo'/></a>
+        </nav>
+        <ConfigProvider>
+          <Button type="primary" className='about-ass-button-footer'>primary</Button>
+        </ConfigProvider>
         
 
       </Footer>
