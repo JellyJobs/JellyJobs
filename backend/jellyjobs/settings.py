217755981a7b038913e489eb4bb34d2be678b17d
Romeo L. Monfroglio <141967584+RomeKai@users.noreply.github.com>
2024-10-10 20:57:46 -0300

    settings

--- backend/jellyjobs/settings.py
+++ backend/jellyjobs/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'JellyJobs'
 ]
 
 MIDDLEWARE = [
@@ -86,6 +88,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
 
 
 # Internationalization
