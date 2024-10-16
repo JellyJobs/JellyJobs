1ae260818d90395cc8075b438322b58edbed30d5
Ivo4774 <iabh2002@gmail.com>
2024-10-16 00:07:06 -0300

    DALE QUE YA ANDA

---
+++ backend/external/externalModels.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from models import Profesion
+from models import Localidad
+from models import Archivo
+from models import Cv
+
+
+
+
--- backend/jellyjobs/settings.py
+++ backend/jellyjobs/settings.py
@@ -151,6 +151,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'jellyjobs',
+    'external',
 ]
 
 MIDDLEWARE = [
